675788218f306629e6c91bfa2d66c5eb161c4861
Saeed Khaled <saeedpop1999@gmail.com>
2024-07-01 19:46:22 +0300

    Last Update '2024/07/01' at 07:46 | Add Seo

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import "./globals.css";
+import SessionProvider from "./SessionProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://lyerz.vercel.app"),
+  title: "LogoJulio - Logos competition",
+  description: "Dev with LogoJulio by Saeed Khaled",
+  openGraph: {
+    title: "LogoJulio - Logos competition",
+    description: "Dev with LogoJulio by Saeed Khaled",
+    images: {
+      url: "/assets/og.jpg",
+      width: 1200,
+      height: 630,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "LogoJulio - Logos competition",
+    description: "Dev with LogoJulio by Saeed Khaled",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
+    </html>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,26 @@
 "use client";
 import NavBar from "./components/NavBar";
 import UploadForm from "./components/UploadForm";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>عنوان التبويبة للصفحة الرئيسية</title>
+        <meta name="description" content="وصف الصفحة الرئيسية" />
+        <meta property="og:title" content="عنوان Open Graph للصفحة الرئيسية" />
+        <meta
+          property="og:description"
+          content="وصف Open Graph للصفحة الرئيسية"
+        />
+        <meta
+          property="og:image"
+          content="رابط صورة Open Graph للصفحة الرئيسية"
+        />
+      </Head>
       <NavBar />
-      <UploadForm />;
+      <UploadForm />
     </>
   );
 }
