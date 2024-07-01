a794b3e4e116b05d678ec1f6ad125aacc76d6fef
Saeed Khaled <saeedpop1999@gmail.com>
2024-07-01 22:17:07 +0300

    Last Update '2024/07/01' at 10:17PM | Add featured

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -3,12 +3,17 @@ import { useEffect, useState } from "react";
 import { auth, firestore } from "/src/firebase";
 import Image from "next/image";
 import Logo from "/public/assets/logo.svg";
+import logout from "/public/assets/logout.svg";
+import addedFeature from "/public/assets/addedFeature.svg";
+import addFeature from "/public/assets/addFeature.svg";
 import {
   collection,
   query,
   where,
   onSnapshot,
   orderBy,
+  doc,
+  updateDoc,
 } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
@@ -16,6 +21,7 @@ import Link from "next/link";
 const AdminDashboard = () => {
   const [images, setImages] = useState([]);
   const [selectedDay, setSelectedDay] = useState("All Days");
+  const [showOnlyFeatured, setShowOnlyFeatured] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -25,7 +31,6 @@ const AdminDashboard = () => {
       } else {
         let q = collection(firestore, "images");
 
-        // Filter by selected day if selectedDay is set and not "All Days"
         if (selectedDay && selectedDay !== "All Days") {
           q = query(q, where("day", "==", selectedDay));
         } else {
@@ -60,6 +65,53 @@ const AdminDashboard = () => {
     setSelectedDay(day);
   };
 
+  const toggleShowOnlyFeatured = () => {
+    setShowOnlyFeatured(!showOnlyFeatured);
+  };
+
+  const addImageToFeatures = async (imageId) => {
+    const imageRef = doc(firestore, "images", imageId);
+    const image = images.find((img) => img.id === imageId);
+
+    try {
+      await updateDoc(imageRef, {
+        isFeatured: !image.isFeatured, // Toggle the value
+      });
+    } catch (error) {
+      console.error("Error updating document:", error);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        router.push("/admin/login");
+      } else {
+        let q = collection(firestore, "images");
+
+        if (selectedDay && selectedDay !== "All Days") {
+          q = query(q, where("day", "==", selectedDay));
+        } else if (showOnlyFeatured) {
+          q = query(q, where("isFeatured", "==", true));
+        } else {
+          q = query(q, orderBy("createdAt", "desc"));
+        }
+
+        const unsubscribeImages = onSnapshot(q, (snapshot) => {
+          const imagesData = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setImages(imagesData);
+        });
+
+        return () => unsubscribeImages();
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router, selectedDay, showOnlyFeatured]);
+
   return (
     <div className="flex items-start justify-center w-screen h-screen overflow-hidden">
       <div className="flex flex-col items-center justify-between h-screen px-8 py-8 border-r bg-neutral-50 w-96">
@@ -93,12 +145,22 @@ const AdminDashboard = () => {
               {`Day ${String(i + 1).padStart(2, "0")}`}
             </button>
           ))}
+          <button
+            className={`flex-1 px-3 py-2 text-sm rounded-lg w-full transition-all duration-200 col-start-1 col-end-3 ${
+              showOnlyFeatured
+                ? "bg-green-500 text-white"
+                : "bg-neutral-200 text-black"
+            }`}
+            onClick={toggleShowOnlyFeatured}
+          >
+            Featured Logos
+          </button>
         </div>
         <button
           onClick={handleLogout}
-          className="px-3 py-1 text-sm text-black rounded-lg bg-neutral-200"
+          className="flex items-center justify-center gap-2 px-3 py-2 text-sm font-medium text-black bg-white border rounded-lg"
         >
-          Logout
+          Logout <Image src={logout} alt="Logout" className="w-4 h-4 " />
         </button>
       </div>
       <div
@@ -110,27 +172,64 @@ const AdminDashboard = () => {
           <div className="text-xl">There are no logos yet!</div>
         ) : (
           <div className="grid w-full grid-cols-1 gap-4 px-8 md:grid-cols-2 lg:grid-cols-4">
-            {images.map((image) => (
-              <div
-                key={image.id}
-                className="relative w-full h-full overflow-hidden rounded-lg"
-              >
-                <Link
-                  href={image.socialAcountLink}
-                  target="_blank"
-                  className="absolute top-0 left-0 z-10 flex items-center justify-center w-full h-full text-white transition-all duration-300 bg-[rgba(0,0,0,0.5)] backdrop-blur-lg opacity-0 hover:opacity-100"
-                >
-                  Visit Profile
-                </Link>
-                <Image
-                  src={image.url}
-                  alt={`Uploaded ${image.id}`}
-                  width={300}
-                  height={300}
-                  className="object-cover w-full h-full aspect-square"
-                />
-              </div>
-            ))}
+            {images.map(
+              (image) =>
+                (!showOnlyFeatured || image.isFeatured) && (
+                  <div
+                    key={image.id}
+                    className="relative w-full h-full overflow-hidden rounded-2xl"
+                  >
+                    <Link
+                      href={image.socialAcountLink}
+                      target="_blank"
+                      className="absolute top-0 left-0 z-10 flex items-center justify-center w-full h-full text-white transition-all duration-300 bg-[rgba(0,0,0,0.5)] backdrop-blur-lg opacity-0 hover:opacity-100"
+                    >
+                      Visit Profile
+                    </Link>
+                    <div
+                      className={`absolute flex items-center  w-[calc(100% - 32px)] bottom-2 left-2 right-2 z-10 ${
+                        selectedDay === "All Days"
+                          ? "justify-between"
+                          : "justify-end"
+                      }`}
+                    >
+                      <p
+                        className={`px-4 h-full py-2 text-xs leading-none text-white bg-[rgba(255,255,255,0.5)] rounded-xl backdrop-blur-lg inline-block ${
+                          selectedDay === "All Days" ? "" : "hidden"
+                        }`}
+                      >
+                        {image.day}
+                      </p>
+                      <div
+                        className="px-4 py-2 text-xs leading-none text-white bg-[rgba(255,255,255,0.5)] rounded-xl backdrop-blur-lg inline-block  cursor-pointer"
+                        onClick={() => addImageToFeatures(image.id)}
+                      >
+                        {!image.isFeatured && (
+                          <Image
+                            src={addFeature}
+                            alt="Add Feature"
+                            className="w-auto h-4"
+                          />
+                        )}
+                        {image.isFeatured && (
+                          <Image
+                            src={addedFeature}
+                            alt="Added Feature"
+                            className="w-auto h-4"
+                          />
+                        )}
+                      </div>
+                    </div>
+                    <Image
+                      src={image.url}
+                      alt={`Uploaded ${image.id}`}
+                      width={300}
+                      height={300}
+                      className="object-cover w-full h-full aspect-square"
+                    />
+                  </div>
+                )
+            )}
           </div>
         )}
       </div>
