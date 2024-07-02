b879c5a9df5cd88c2641728414fdb77f169bea57
Saeed Khaled <saeedpop1999@gmail.com>
2024-07-03 00:31:08 +0300

    Last Update '2024/07/02' at 12:31PM | edits

--- src/app/page.js
+++ src/app/page.js
@@ -6,19 +6,6 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>عنوان التبويبة للصفحة الرئيسية</title>
-        <meta name="description" content="وصف الصفحة الرئيسية" />
-        <meta property="og:title" content="عنوان Open Graph للصفحة الرئيسية" />
-        <meta
-          property="og:description"
-          content="وصف Open Graph للصفحة الرئيسية"
-        />
-        <meta
-          property="og:image"
-          content="رابط صورة Open Graph للصفحة الرئيسية"
-        />
-      </Head>
       <NavBar />
       <UploadForm />
     </>
